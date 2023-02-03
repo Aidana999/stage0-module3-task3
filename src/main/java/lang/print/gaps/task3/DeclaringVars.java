e1268a240e98bfe422448d6cb0017743ba4f9bd2
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-02-03 22:54:37 +0600

    Update DeclaringVars.java

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,23 +2,7 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-
-        int linkToFirst = first;
-        int linkToSecond = second;
-        int linkToThird = third;
-
-        linkToFirst = 15;
-        linkToSecond = 6;
-        linkToThird = 4;
-
-        System.out.println(linkToFirst);
-        System.out.println(linkToSecond);
-        System.out.println(linkToThird);
+        int a, b, c = 10;
+        System.out.println(c);
     }
 }
